c305f421611ea43be597ba52e1728f8bd9df0cc9
Bah Ousmane <ousmane.bah1983@gmail.com>
2017-02-08 17:39:30 +0100

    add header et footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Navbar from './navbar/Navbar.jsx';
+import Footer from './footer/Footer.jsx';
 
 class App extends Component {
   render() {
     return (
       <div>
+      	<Navbar />
+      	<Footer />
       </div>
     );
   }
---
+++ my-app/src/navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import './Navbar.css';
+
+import Panier from '../img/panier.jpg'
+
+
+class Navbar extends React.Component {
+
+	render(){
+		return(
+			<div className="container">
+
+				<div className="logo">
+
+				    <a href="#" className="logotexte">La bonne bouffe</a>
+
+			    </div>
+
+				<div className="menu">
+					<ul>
+						<li><a href="#">Accueil</a></li>
+						<li><a href="#">Carte</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</div>
+
+				<button className="panier">
+					<a href="#"><img src={Panier} alt="le panier du client"/></a>
+					<a href="#" className="monnaie">0 €</a>
+				</button>
+
+			</div>
+	    )
+
+	}
+}
+
+// J'autorise l'exportation du fichier Navbar vers le App !
+
+export default Navbar;
