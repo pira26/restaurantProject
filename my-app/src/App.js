7823260669381c91b1b5d115d0d7a2472c15d014
dounia <d.fergane@gmail.com>
2017-02-08 18:31:30 +0100

    add carousel

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Carrousel from './carrousel/Carrousel.jsx';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
+        <Carrousel />
+     
       </div>
     );
   }
