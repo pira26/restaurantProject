7af5aee7206c4242ee7ba2cd3afa11b9be6886df
loic-belaid <loicbere@gmail.com>
2017-02-08 16:20:24 +0100

    add Card & Map position

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Card from './Card/Card.jsx';
+import Map from './Map/Map.jsx';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Card />
+        <Map />
       </div>
     );
   }
