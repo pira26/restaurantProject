385d769f46191cd9313d1d1177f9c189ef6b9f0b
Pirathaab <sritharan.pirathaab@gmail.com>
2017-02-09 17:56:07 +0100

    uptade files

--- my-app/src/Map/Map.jsx
+++ my-app/src/Map/Map.jsx
@@ -4,33 +4,39 @@ import './Map.css';
 
 class Map extends React.Component {
 	componentDidMount() {
-    // fetch("https://maps.googleapis.com/maps/api/geocode/json?address=51+Rue+de+Vincennes+93100+Montreuil,+FR&key=AIzaSyD-6XvCVeVBRstAnJv62TWu1HYqICn42Ko").then(function(response) {
-    //   var contentType = response.headers.get("content-type");
-    //   if(contentType && contentType.indexOf("application/json") !== -1) {
-    //     return response.json().then(function(json) {
-    //     // traitement du JSON
-    //     });
-    //   } else {
-    //     console.log("Oops, nous n'avons pas du JSON!");
-    // }
-    const myLatLng = {lat: 48.866667, lng: 2.333333};
+    const myLatLng = {lat: 48.8541282, lng: 2.436822};
 		const map = new window.google.maps.Map(this.refs.mapCanvas, {
       center: myLatLng,
     	zoom: 12
   	});
-    const marker = new window.google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      title: "La Bonne Bouffe"
-    });
-	}
+    fetch("https://maps.googleapis.com/maps/api/geocode/json?address=51+Rue+de+Vincennes+93100+Montreuil,+FR&key=AIzaSyD-6XvCVeVBRstAnJv62TWu1HYqICn42Ko").then(function(response){
+      var contentType = response.headers.get("content-type");
+      if(contentType && contentType.indexOf("application/json") !== -1) {
+        return response.json().then(function(json) {
+          console.log('json:'+ JSON.stringify(json));
+          return json.results[0].geometry.location; 
+        // traitement du JSON
+        });
+      } 
+      else {
+        console.log("Oops, nous n'avons pas du JSON!");
+      }
+    }).then(function(location){
+        console.log('location:' + location.lat);
+        const marker = new window.google.maps.Marker({
+        position: {lat: location.lat, lng: location.lgn},
+        map: map,
+        title: "La Bonne Bouffe"
+        });
+      });
 
+  }
   render() {
 
     return (
       <div>
         <div className="titleMap"><i className="fa fa-map-marker" aria-hidden="true"></i> Ou nous trouver:</div>
-        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div className="col-xs-6 col-sm-6 col-md-6 col-lg-12">
           <div ref="mapCanvas" style={{width:'100%', height:'400px'}}></div>
         </div>
      </div>
