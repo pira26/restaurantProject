dcb0d6093fd6c95644f124cd5aa9e06e320c25ca
loic-belaid <loicbere@gmail.com>
2017-02-09 14:25:38 +0100

    update cards

--- my-app/src/Card/Card.jsx
+++ my-app/src/Card/Card.jsx
@@ -10,30 +10,6 @@ class Card extends React.Component {
     return (
 
       <div>
-       { /*<div className="Card">
-               <div className="img img1"></div>
-               <div className="text">
-                 <div className="title">Poulet au poulet 100% poulet<span className="price">17,20€</span></div>
-                 <div className="desc">Un grand classique le poulet de Bresse aux morilles et à la crème. Si vous ne trouvez pas de volaille de Bresse, choisissez un bon poulet fermier, élevé aux grains et qui a vécu en liberté dans les champs.</div>
-               </div>
-               <div className="add"><i className="fa fa-plus-circle" aria-hidden="true"></i></div>
-             </div>
-             <div className="Card">
-               <div className="img img2"></div>
-               <div className="text">
-                 <div className="title">Poulet au poulet 100% poulet<span className="price">17,20€</span></div>
-                 <div className="desc">Un grand classique le poulet de Bresse aux morilles et à la crème. Si vous ne trouvez pas de volaille de Bresse, choisissez un bon poulet fermier, élevé aux grains et qui a vécu en liberté dans les champs.</div>
-               </div>
-               <div className="add"><i className="fa fa-plus-circle" aria-hidden="true"></i></div>
-             </div>
-             <div className="Card">
-               <div className="img img3"></div>
-               <div className="text">
-                 <div className="title">Poulet au poulet 100% poulet<span className="price">17,20€</span></div>
-                 <div className="desc">Un grand classique le poulet de Bresse aux morilles et à la crème. Si vous ne trouvez pas de volaille de Bresse, choisissez un bon poulet fermier, élevé aux grains et qui a vécu en liberté dans les champs.</div>
-               </div>
-               <div className="add"><i className="fa fa-plus-circle" aria-hidden="true"></i></div>
-             </div> */}
           { contenu.carte.map(menu =>
             <div className="Card">
             <img src={menu.image} className="img"/>
