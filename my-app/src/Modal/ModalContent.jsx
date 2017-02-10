58bea4e998d859df77b32c906c586b405cef4220
dounia <d.fergane@gmail.com>
2017-02-10 12:08:52 +0100

    stylize modal

--- my-app/src/Modal/ModalContent.jsx
+++ my-app/src/Modal/ModalContent.jsx
@@ -9,18 +9,47 @@ class ModalContent extends React.Component {
       <div className="content">
               <div className="monPanier">Mon Panier</div>
                 <div className="choiceContent">
+                <div className="choiceWrapper">
                   <div className="plat">Ma commande
-                    <ul>
-                      <li>Plat</li>
-                    </ul>
+                      <ul>
+                        <li>Plat</li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                      </ul>
                   </div>
-                  <div className="quantite">Quantités</div>
-                  <div className="prix">Prix</div>
-                  <div className="delete">Suppr</div>
-                </div>
-                <div className="total"></div>
-                <button className="cmd">Commander</button>
+                  <div className="quantite">Quantités
+                      <ul>
+                        <li>0</li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                      </ul>
+                  </div>
+                  <div className="prix">Prix
+                      <ul>
+                        <li>0</li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                    </div>
+                  <div className="delete">Suppr
+                      <ul>
+                        <li>X</li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                      </ul>
+                    </div>
+              </div>
+                  <div className="total">
+                        <h2>Total</h2>
+                  </div>
+                
           </div>
+          <button className="cmd">Commander</button>
+      </div>
     )
   }
 }
